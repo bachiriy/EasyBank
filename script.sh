b4d5ab7cd399223ac25e17416923a4f634bc1cd0
Mohammed El Bachiri <mohammed-el-bachiri@users.noreply.github.com>
2025-02-18 16:44:13 +0100

    ignore script.sh

--- script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-service_name=$1
-
-mkdir "${service_name}-service"
-
-touch "${service_name}-service/README.md"
-
-echo "# ${service_name} Service" >> "${service_name}-service/README.md"
