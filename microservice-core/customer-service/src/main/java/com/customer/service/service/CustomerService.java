837c1a859577c0ad315b690fcd528cb4a7b752f5
Mohammed El Bachiri <mohammed-el-bachiri@users.noreply.github.com>
2025-03-01 16:20:56 +0000

    add customer udpdate/delete

--- microservice-core/customer-service/src/main/java/com/customer/service/service/CustomerService.java
+++ microservice-core/customer-service/src/main/java/com/customer/service/service/CustomerService.java
@@ -3,10 +3,13 @@ package com.customer.service.service;
 import java.util.List;
 import com.customer.service.dto.request.CustomerRequest;
 import com.customer.service.dto.response.CustomerResponse;
+import org.springframework.http.ResponseEntity; 
 
 public interface CustomerService {
    List<CustomerResponse> getCustomers();
    CustomerResponse addCustomer(CustomerRequest customer);
    CustomerResponse getCustomerById(Long id);
+   CustomerResponse updateCustomer(Long id, CustomerRequest customer);
+   ResponseEntity<String> deleteCustomer(Long id);
 }
 
--- microservice-core/customer-service/src/main/java/com/customer/service/service/impl/CustomerServiceImpl.java
+++ microservice-core/customer-service/src/main/java/com/customer/service/service/impl/CustomerServiceImpl.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity; 
 
 import com.customer.service.dto.request.CustomerRequest;
 import com.customer.service.dto.response.CustomerResponse;
@@ -14,6 +16,7 @@ import com.customer.service.exceptions.ResourceAlreadyExistsException;
 import com.customer.service.exceptions.ResourceNotFoundException;
 import com.customer.service.repository.CustomerRepository;
 import com.customer.service.mapper.CustomerMapper;
+import com.customer.service.provider.RestTemplateProvider;
 
 
 @Service
@@ -35,5 +38,35 @@ public class CustomerServiceImpl implements CustomerService {
         if (customerFound.isPresent()) {
             return this.customerMapper.map(customerFound.get());
         } else throw new ResourceNotFoundException("Customer with this ID does not exists."); 
+    }
+
+    public CustomerResponse updateCustomer(Long id, CustomerRequest customer){
+        Optional<Customer> dbCustomer = this.customerRepository.findById(id);
+        if (dbCustomer.isPresent()) {
+            Customer customerToSave = customerMapper.customerRequestToCustomer(customer);
+            return customerMapper.map(
+                    customerRepository.save(
+                        Customer.builder()
+                        .id(dbCustomer.get().getId())
+                        .name(!empty(customerToSave.getName()) ? customerToSave.getName() : dbCustomer.get().getName())
+                        .email(!empty(customerToSave.getEmail()) ? customerToSave.getEmail() : dbCustomer.get().getEmail())
+                        .build()));
+        } else throw new ResourceNotFoundException("Customer with this ID does not exists."); 
+    }
+
+    public ResponseEntity<String> deleteCustomer(Long id){
+        Optional<Customer> dbCustomer = this.customerRepository.findById(id);
+        if (dbCustomer.isPresent()) {
+            boolean deletedCustomerAccounts = RestTemplateProvider.deleteCustomerAccounts(id);
+            if (deletedCustomerAccounts) {
+                customerRepository.deleteById(id);
+                return new ResponseEntity<String>("Customer was deleted with success", HttpStatus.OK);
+            } else return new ResponseEntity<String>("Failed deleting customer.", HttpStatus.BAD_REQUEST);
+        } else throw new ResourceNotFoundException("Customer with this ID does not exists."); 
+    }
+
+
+    private boolean empty(String s) {
+    return s == null || s.trim().isEmpty();
     }
 }
